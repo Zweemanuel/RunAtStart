2a49a7c6433b7867617e1e952fc23079ed14c631
Zweemanuel <79989883+Zweemanuel@users.noreply.github.com>
2022-09-06 00:07:48 +0200

    Implemented code to ensure applications exist once

--- src/main/java/com/runatstart/App.java
+++ src/main/java/com/runatstart/App.java
@@ -20,6 +20,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 910, 620);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- src/main/java/com/runatstart/PrimaryController.java
+++ src/main/java/com/runatstart/PrimaryController.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 
 public class PrimaryController implements Initializable{
 
@@ -14,16 +17,25 @@ public class PrimaryController implements Initializable{
     private ListView<File> allItems;
     @FXML
     private ListView<File> startItems;
-
+    
     @FXML
-    private void showItems(String pathname, ListView<File> listV) throws IOException {
+    private void showItems(String pathname, ListView<File> listV) throws IOException { //TODO: do not add x to allItems if already in startItems
+        Boolean found = false;
         File f = new File(pathname);
         File[] fileList = f.listFiles();
         for (File file : fileList) {
             
-            if(file.isFile()){
-                System.out.println(file.getName());
-                listV.getItems().add(file);
+            if(file.isFile()){//FIX fix this, why is file not found???
+                for(File fileS : allItems.getItems()){
+                    if(file.getName()==fileS.getName()){
+                        found=true;
+                        System.out.println("FOUND");
+                    }
+                }
+                if(!found){
+                    listV.getItems().add(file);
+                }
+                
             }else{
                 showItems(pathname+"/"+file.getName(),listV);
             }
@@ -31,13 +43,17 @@ public class PrimaryController implements Initializable{
     }
 
     
+
+    
     @FXML
     private void RefreshLists() throws IOException {
         allItems.getItems().clear();
         startItems.getItems().clear();
+        
         showItems("C:/ProgramData/Microsoft/Windows/Start Menu/Programs",allItems);
-        showItems("C:/ProgramData/Microsoft/Windows/Start Menu/Programs",startItems);
-        System.out.println("Lists cleared");
+        showItems("C:/ProgramData/Microsoft/Windows/Start Menu/Programs/startup",startItems);
+
+        System.out.println("Lists refreshed");
         
     }
 
@@ -49,11 +65,27 @@ public class PrimaryController implements Initializable{
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         try {
-            this.showItems("C:/ProgramData/Microsoft/Windows/Start Menu/Programs",allItems);
-            this.showItems("C:/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp",startItems);
+            RefreshLists();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        allItems.setOnMouseClicked((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent click) {//Copies the clicked file to the startup folder and removes it from the list
+                
+                if (click.getClickCount() >= 2) {
+                   //Get current selected item
+                   File currentItemSelected = allItems.getSelectionModel().getSelectedItem();
+                   
+                    startItems.getItems().add(currentItemSelected);
+                    allItems.getItems().remove(currentItemSelected);
+                   
+                   
+                }
+            }
+        });
     }
 
 }
